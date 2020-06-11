75c6fee98700b61bf79cba9e9f91fc1297881d63
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-06-11 15:03:17 +0900

    Cat, Dog 클래스

--- inheritancePrac/Program.cs
+++ inheritancePrac/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace inheritancePrac
+namespace InheritancePrac
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<Dog> dogs =
+                new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> cats = new List<Cat>();
+            cats.Add(new Cat());
+            cats.Add(new Cat());
+            cats.Add(new Cat());
+
+            foreach (var item in dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+
+            foreach (var item in cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
         }
     }
 }
